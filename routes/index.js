8396181e57ed379bd93eb2391f429bf535583004
Ahmet Erdem ÖZTÜRK <ahmeterdemozturk30@gmail.com>
2026-05-02 13:56:16 +0300

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pageController = require("../controllers/pageController");
+const pageController = require("../temp_kontrol/pageController");
 
 router.get("/", pageController.getIndexPage);
 router.post("/contact", pageController.submitContactForm);
